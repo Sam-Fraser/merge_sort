424cc0c859d38237bbfc3c9af3890657fa4505f4
Sam Eff <HeySamEff@gmail.com>
2020-06-18 12:51:52 -0700

    added description to README.md and created psuedocode for method

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # merge_sort
+
+Merge sort project from the Odin Project. Find the explanation here (https://www.theodinproject.com/lessons/recursion)
+
---
+++ main.rb
@@ -0,0 +1,8 @@
+#merge_sort method
+#on input of n elements
+  #return n if n < 2
+  #else
+    #sort left half
+    #sort right half
+    #merge the sorted halves
+
